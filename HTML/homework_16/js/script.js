9001f14a5dc16d3750079c2416815846a13e82f1
Yuliya Payevska <petrikjul@gmail.com>
2018-01-02 15:01:38 +0200

    homework_16 doing section-2

---
+++ HTML/homework_16/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var a = 4;
+
+function name() {
+
+}
+
+var b = name();
+
+var c = 5;
+
+var arr = [1, 2, 3, 4, 5];
+
+arr.shift();
+
+console.log("aaaa");
+
+var name = "a";
+name.length;
